40d43ae9a98ca7046f017632e95ad1a389679018
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-11-28 22:54:29 +0530

    trying to make basic auth work

--- src/main/java/com/friendsbook/user/security/SecurityCenter.java
+++ src/main/java/com/friendsbook/user/security/SecurityCenter.java
@@ -1,7 +1,9 @@
 package com.friendsbook.user.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,14 +15,30 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityCenter extends WebSecurityConfigurerAdapter{
 	
-	// Currently I'm using basic auth as it's the easiest to implement
+	@Value("${SECURITY-USER-NAME}")
+	private String basicSecurityUser;
+	@Value("${SECURITY-USER-PASSWORD}")
+	private String basicSecurityPassword;
+	
+	// Currently I'm using basic auth as it's the easiest to implement.
 	// will update in future to more reliable methods which can be used among microservices
-	// like validation using keys
+	// like 'validation using keys'
 	// please note OAuth is not valid here as token will expire after some time and this being
 	// an internal microservice(and not external) we cannot generate token again and again
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
+		http.authorizeRequests()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.httpBasic();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser(basicSecurityUser)
+			.password(this.passwordEncoder().encode(basicSecurityPassword));
 	}
 	
 	// makes certain APIs public
