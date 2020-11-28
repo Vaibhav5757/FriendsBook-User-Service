a7e037bfe15f5eb89ff244c7ad998b280672b1a5
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-11-28 22:42:31 +0530

    made changes to security configuration

--- src/main/java/com/friendsbook/user/security/SecurityCenter.java
+++ src/main/java/com/friendsbook/user/security/SecurityCenter.java
@@ -3,6 +3,7 @@ package com.friendsbook.user.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,11 +21,14 @@ public class SecurityCenter extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
-		http.authorizeRequests()// authorize requests
-			.anyRequest()// any request
-			.authenticated()// and authenticate
-			.and()
-			.httpBasic();// use http basic for authentication
+	}
+	
+	// makes certain APIs public
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+			.antMatchers("/wake-up**")
+			.antMatchers("/user/sign-up");
 	}
 	
 	@Bean
