83ac8c28f9535a839810fde7e67c9cd19b91e010
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-11-28 22:00:09 +0530

    added wake up call controller

---
+++ src/main/java/com/friendsbook/user/controller/RootController.java
@@ -0,0 +1,19 @@
+package com.friendsbook.user.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/wake-up")
+public class RootController {
+	
+	private Logger logger = LoggerFactory.getLogger(RootController.class);
+
+	@GetMapping("/")
+	public void wakeUp() {
+		logger.info("Wake up call recieved!!!");
+	}
+}
