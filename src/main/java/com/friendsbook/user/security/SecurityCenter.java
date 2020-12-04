49a86c0b1a35c64d02c7b9762efea586a253df29
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-12-04 23:06:33 +0530

    removed websecurity method

--- src/main/java/com/friendsbook/user/security/SecurityCenter.java
+++ src/main/java/com/friendsbook/user/security/SecurityCenter.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -42,14 +41,6 @@ public class SecurityCenter extends WebSecurityConfigurerAdapter{
 			.withUser(basicSecurityUser)
 			.password(this.passwordEncoder().encode(basicSecurityPassword))// encode the password
 			.roles("SECURITY_HEAD");
-	}
-	
-	// makes certain APIs public
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web
-			.ignoring()
-			.antMatchers("/wake-up/**");
 	}
 	
 	@Bean
